29417d650eefdf61cbb1a9fde3ceec8ce6e61448
alex_man <alexmnsbs@gmail.com>
2023-09-17 10:37:56 +0300

    add NestedTest

---
+++ 05-nested-classes/begin/NestedTest.java
@@ -0,0 +1,11 @@
+package begin;
+
+public class NestedTest {
+
+    static class A { }
+    class B { }
+    void metod(){
+        class C{}//локальный класс, можно создавать и внутри блока
+    }
+    
+}
