d242ff2f4f9fb04d4263df1e4b6403179deeadc6
alex_man <alexmnsbs@gmail.com>
2024-02-20 19:04:18 +0300

    add SynchronizedCollectionSyncTest

--- 08-multithreading/threadCollections/SynchronizedCollectionSyncTest.java
+++ 08-multithreading/threadCollections/SynchronizedCollectionSyncTest.java
@@ -1,17 +1,20 @@
 package threadCollections;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class SynchronizedCollectionTest {
-
+public class SynchronizedCollectionSyncTest {
+    
     public static void main(String[] args) throws InterruptedException {
 
         ArrayList<Integer> source = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 5; i++) {
             source.add(i);            
         }
-        ArrayList<Integer> target = new ArrayList<>();
-        Runnable runnable = () -> target.addAll(source);
+        List<Integer> syncList = Collections.synchronizedList(new ArrayList<>());
+
+        Runnable runnable = () -> syncList.addAll(source);
 
         Thread thread1 = new Thread(runnable);
         Thread thread2 = new Thread(runnable);
@@ -19,7 +22,8 @@ public class SynchronizedCollectionTest {
         thread2.start();
         thread1.join();
         thread2.join();
-        System.out.println(target);
-    }
-    
+        System.out.println(syncList);
+
+    }    
+
 }
